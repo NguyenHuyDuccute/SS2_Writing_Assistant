e4ffd2d8454e01def7859948bbef5b502de0c827
NguyenHuyDuccute <2001140013@s.hanu.edu.vn>
2023-05-01 03:09:38 +0700

    login logout

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
 import os
+from functools import wraps
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, session
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
+app.secret_key = 'random secret'
+# openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
+# oauth config
+oauth = OAuth(app)
+app.config['GOOGLE_CLIENT_ID'] = "563039761492-tr2s6c24bjps7ks2f69ja8f4oje940kj.apps.googleusercontent.com"
+app.config['GOOGLE_CLIENT_SECRET'] = "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"
+
+google = oauth.register(
+    name='google',
+    client_id=app.config["GOOGLE_CLIENT_ID"],
+    client_secret=app.config["GOOGLE_CLIENT_SECRET"],
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={
+        'scope': 'openid email profile',
+        'jwks_uri': 'https://www.googleapis.com/oauth2/v3/certs'
+    },
+    jwks_uri='https://www.googleapis.com/oauth2/v3/certs'
+
+)
+
+
+def login_required(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if 'email' not in session:
+            session['next'] = request.url
+            return redirect(url_for('google_login'))
+        return func(*args, **kwargs)
+
+    return decorated_view
 
 
 @app.route("/grammar-check", methods=['GET', 'POST'])
-def check_grammar():
+@login_required
+def grammar_check():
     if request.method == "POST":
         input_text = request.form['text']
         response = openai.Completion.create(
@@ -21,8 +58,10 @@ def check_grammar():
             presence_penalty=0.0
         )
         corrected_text = response.choices[0].text.strip()
-        return render_template('grammar_check.html', corrected_text=corrected_text)
-    return render_template('grammar_check.html')
+        return render_template('grammar_check.html', corrected_text=corrected_text, is_logged_in=True)
+    return render_template('grammar_check.html', is_logged_in=True)
+
+
 # Correct this to standard English
 
 @app.route("/grammar-check-post", methods=['POST'])
@@ -42,40 +81,41 @@ def test():
         return corrected_text
 
 
-@app.route("/", methods=("GET", "POST"))
+@app.route('/')
 def index():
-    if request.method == "POST":
-        paraphrase = request.form["paraphrase"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(paraphrase),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+    if 'email' in session:
+        return render_template('base.html', is_logged_in=True)
+    else:
+        return render_template('base.html', is_logged_in=False)
 
 
 @app.route("/text-completion", methods=(['GET', 'POST']))
+@login_required
 def text_completion():
-    return render_template("text_completion.html")
+    return render_template('text_completion.html', is_logged_in=True)
+
+@app.route('/login')
+def google_login():
+    google = oauth.create_client('google')
+    redirect_uri = url_for('google_authorize', _external=True)
+    next_url = request.args.get('next', None)
+    if next_url:
+        redirect_uri += f'?next={next_url}'
+    return google.authorize_redirect(redirect_uri)
 
 
-def generate_prompt(paraphrase):
-    prompt_dict = {
-        "Hulu has a big user base.": "Hulu commands a sizeable market.",
-        "Milk is good for your bones.": "Milk strengthens one’s bones.",
-        "GMOs are seen as bad, but that’s not accurate.": "GMOs command a negative reputation, but this sentiment is "
-                                                          "inaccurate."
-    }
-    rewrite = prompt_dict.get(paraphrase)
-    if not rewrite:
-        return None
-    keywords = {
-        "Hulu commands a sizeable market.": ["wields", "reach"],
-        "Milk strengthens one’s bones.": ["documented", "bolster"],
-        "GMOs command a negative reputation, but this sentiment is inaccurate.": ["tainted", "inaccurate"]
-    }
-    prompt = f"Rewrite: (Keywords: {', '.join(keywords[paraphrase])}) {rewrite} "
-    return prompt
+@app.route('/authorize')
+def google_authorize():
+    google = oauth.create_client('google')
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    session['email'] = user_info['email']
+    next_url = session.get('next') or url_for('index')
+    return redirect(next_url)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
